dcb7686dd1f644695f452b05c704271f9ad96c65
MartinRGB <qiuyinsen@gmail.com>
2022-11-03 20:31:35 +0800

    delete old script & add scrcpy script
    delete old script & add scrcpy script

--- cli/win_script/old/run_backup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
-echo LAN Address: http://%NetworkIP%:3000/
-cmd /k "npx kill-port 3000 && adb reverse tcp:3000 tcp:3000 && adb shell am start -a android.intent.action.VIEW -d http://localhost:3000 && start chrome.exe http://%NetworkIP%:3000/ && serve"
-pause
--- cli/win_script/old/scrcpy.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cmd /k "adb shell am broadcast -a com.oculus.vrpowermanager.prox_close && scrcpy --bit-rate 25M --crop 1390:1470:170:180"
-pause
