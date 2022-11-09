afa22e13dfbee3e90d83cf516fe20cc3ea976e83
MartinRGB <qiuyinsen@gmail.com>
2022-11-09 13:26:17 +0800

    update batch script
    update batch script

---
+++ cli/win_script/promixi_censor_off.bat
@@ -0,0 +1,3 @@
+@echo off
+cmd /k "adb shell am broadcast -a com.oculus.vrpowermanager.prox_close"
+pause
--- cli/win_script/scrcpy.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
-echo LAN Address: http://%NetworkIP%:3000/
-cmd /k "adb shell am broadcast -a com.oculus.vrpowermanager.prox_close && scrcpy --bit-rate 25M --crop 1390:1470:170:180"
-pause
